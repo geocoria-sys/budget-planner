c1cea31d0765acba9949a1cc7fb103c319ab2f05
ka-ous <kaousley@iu.edu>
2025-12-08 16:43:15 -0500

    Create BudgetData class for income and expenses management
    This class serves as the central data store for the application, holding income and expenses.

---
+++ BudgetData.java
@@ -0,0 +1,39 @@
+package GroupProject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Project: Smart Budget Planner
+ * Author: Genesis Coria, Harrison Usigbe, Bryit Sumner, Kayla Sturgeon
+ * Date: 12/05/25
+ * This class serves as the central data store for the application, holding income and expenses.
+ */
+public class BudgetData {
+    
+    // Core Attributes
+    private double monthlyIncome;
+    private List<Expense> expenses;
+
+  
+    public BudgetData() {
+        this.monthlyIncome = 0.0;
+        this.expenses = new ArrayList<>();
+    }
+
+    
+    // Retrieves the current monthly income
+    public double getMonthlyIncome() {
+        return monthlyIncome;
+    }
+
+    // Sets the monthly income
+    public void setMonthlyIncome(double monthlyIncome) {
+        this.monthlyIncome = monthlyIncome;
+    }
+
+    // Retrieves the list of expenses (for adding/viewing)
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+}
